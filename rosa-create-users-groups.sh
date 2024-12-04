33321bb52048de048cc004a12fa08a3c37321048
Daniel Schimpfoessl <dds@redhat.com>
2024-12-04 10:23:02 -0600

    Adding fully automated demo system setup script

--- rosa-create-users-groups.sh
+++ rosa-create-users-groups.sh
@@ -1,11 +1,14 @@
+# accepts number of teams as argument
+numberTeams=$1
+numberTeams="${numberTeams:-30}"
 teamname=(redhat achievement ambition brightness brilliance courage creativity excellence flair genius growth independence innovation intelligence leadership mastery opportunity outcome performance philosophy possibility progress resilience shrewdness skill success talent television vision wisdom wit) 
 username=(daniel dean anthony rh1 rh2 rh3 rh4 rh5 rh6 rh7 actor akita alligator alpaca anaconda apple armadillo badger basis bat beach bear beaver bison blast blaze blend blink blood bobcat bonobo bonus boost brand brave bread brick brisk brush bunny burst bushbaby butterfly buyer calf camel carve cat charm cheek cheer cheetah chest chicken child chill chimp chinchilla chinook clam clash claw clear click climb clock cobra corgi cougar cow coyote crab crack craft crane creek crest crisp crocodile crown crush cub daisy dance death deer depth dog drain drama dream drift drink drive duck eagle elephant entry ermine error event falcon fawn ferret flair flame flare flash flick flute focus force fox frog frost froze garter gerbil giant giraffe glare gleam glide glint globe goat gopher gorilla grape grasp grill grizzly grove guest hare hawk heart hedgehog honey horse hotel ibex impala jackal jaguar jellyfish jump kangaroo kid koala lamb leap lemming lemon lemur light lion lizard llama lobster lucky lynx magic marmoset marmot marten match media meerkat mink mole mongoose month moose mount mouse movie music muskrat nerve night octopus opossum orangutan otter owner panda paper peacock pearl phone photo piano pig pika pizza plane plant platypus pluck plumb plume polar poodle porcupine possum power pride pronghorn pulse puma pup python quagga queen quest quilt rabbit raccoon rapid rat ratio river roar rush sable salad scale scene scope scout seahorse shark sharp sheep shift shih shine shirt shock shout shrew skill skunk sleep slice smile smoke snake sonic spade spaniel spark spear speed spike stack stair starfish start steak stoat stone storm story strip surge sweet swift swirl swoop sword table tapir tasmani thing tiger toad tooth topic trail trend trick trunk truth turkey turtle tzu uncle union video virus vivid vocal vole wallaby waver weasel whale wheat whirl wolf wolverine woman wombat world wrist yak youth zebra)
-# echo "$WORDS" | tr ' ' '\n' | awk '{ print length, $0 }' | sort -n | cut -d" " -f2 | uniq | head -n 30 | sort | xargs
+# echo "$WORDS" | tr ' ' '\n' | awk '{ print length, $0 }' | sort -n | cut -d" " -f2 | uniq | head -n ${numberTeams} | sort | xargs
 
 # test our data
-for i in {0..30}; do
+for i in $(seq 0 $numberTeams); do
   team=${teamname[$i]}
-	printf "%-15s" "$team"
+  printf "%-15s" "$team"
   users=$(for u in {0..9}; do index=$((u + i * 10)); printf "%4d: %-10s " "$index" "${username[$index]}"; done)
   echo "$users"
 done
@@ -18,17 +21,17 @@ rosa delete idp --cluster=rosa-$GUID $provider -y
 oc get identity -o jsonpath='{range .items[?(@.providerName=="ai-hacker")]}{.metadata.name}{"\n"}{end}' | xargs -i oc delete identity {}
 oc get users -o name | grep -v admin | xargs -i oc delete {}
 oc delete project -l app.kubernetes.io/part-of=che.eclipse.org
-for i in {0..30}; do
+for i in $(seq 0 $numberTeams); do
   team=${teamname[$i]}
   oc delete group $team
   oc delete project $team
 done
-for i in {0..30}; do
+for i in $(seq 0 $numberTeams); do
   team=${teamname[$i]}
   login="scratch/$team.users.csv"; 
   echo $console > $login;
   echo $api >> $login;
-	echo 'group,username,password' >> $login;
+  echo 'group,username,password' >> $login;
   users=$(for u in {0..9}; do echo -n "${username[$((u + i * 10))]} "; done)
   echo '{"apiVersion":"v1","kind":"Namespace","metadata":{"name":"'$team'","labels":{"event":"hmw","opendatahub.io/dashboard":"true"}}}' | oc create -f -
   oc adm groups new $team; 
@@ -42,7 +45,7 @@ for i in {0..30}; do
   done
 done
 rosa create idp --cluster=rosa-$GUID --name $provider --type htpasswd --from-file $auth
-for i in {0..30}; do
+for i in $(seq 0 $numberTeams); do
   team=${teamname[$i]}
   login="scratch/$team.users.csv"; 
   echo -n 'MinIO-Root,' >> $login;
@@ -50,16 +53,3 @@ for i in {0..30}; do
 done
 # confirm setup
 egrep -i 'https|minio' scratch/*
-
-exit 0;
-
-rosa delete idp --cluster=rosa-$GUID bootcamp -y
-rosa create idp --cluster=rosa-$GUID --name bootcamp --type htpasswd --from-file scratch/users.htpasswd
-oc adm groups add-users cluster-admin admin
-
-for i in $(seq -f"%02g" 1 10); do
-  team=team$i
-  oc delete group $team
-done
-rosa list idp -c rosa-$GUID | egrep -v 'NAME|admin' | cut -d' ' -f1 | xargs -i rosa delete idp -c rosa-$GUID {} -y
-
--- setup.sh
+++ setup.sh
@@ -1,61 +1,123 @@
-# ssh to bastion using -A flag
-BASTION=bastion.t8j8w.sandbox2873.opentlc.com
-ssh-copy-id rosa@$BASTION
-ssh -A rosa@$BASTION
-# rosa login for oc cli
-rosa create admin --cluster rosa-$GUID
-# setup env
-API_URL=https://api.rosa-t8j8w.ft2c.p3.openshiftapps.com:443
-if [ -z "$API_PWD" ]; then read -sp "Enter: " API_PWD; fi
-oc login -u cluster-admin -p "$API_PWD" "$API_URL"
-# Checkout dependencies
-git clone git@github.com:purefield-demo-team/hackmidwest-ai-challenge.git
-cd hackmidwest-ai-challenge
-# Setup dependencies
-mkdir -p scratch/
-# Add intel-amx machine pool
-rosa create machinepool -c rosa-$GUID --name=intel-amx --min-replicas=3 --max-replicas=10 --instance-type=m7i.8xlarge --enable-autoscaling --labels nodes=amx
-# Upgrade Cluster to latest version
-rosa list versions  | sort -nr | head
-rosa upgrade cluster -c rosa-$GUID --control-plane --schedule-date $(date -d "+5 minutes 30 seconds" +"%Y-%m-%d") --schedule-time $(date -d "+6 minutes" +"%H:%M") -m auto -y --version 4.16.11
-watch rosa list upgrades -c rosa-$GUID
-# wait for cluster upgrade to finish
-# wait for machinepool to be ready
-oc wait --for=jsonpath='{.status.phase}'=Active node -l nodes=amx
-rosa list machinepools -c rosa-$GUID
-rosa update machinepool -c rosa-$GUID --replicas 0 workers
-# Have a default storage class
-# Install Operators
-## Web Terminal Operator
-oc apply -f configs/web-terminal-subscription.yaml
-## OpenShift Service Mesh
-oc create ns istio-system
-oc create -f configs/servicemesh-subscription.yaml
-## OpenShift Serverless
-oc create -f configs/serverless-operator.yaml
-## Authorino
-oc create -f configs/authorino-subscription.yaml
-# Verify dependencies
-oc get subscriptions -A
-## OpenShift AI >2.11 via OLM on ROSA
-oc create -f configs/rhoai-operator-ns.yaml
-oc create -f configs/rhoai-operator-group.yaml
-oc create -f configs/rhoai-operator-subscription.yaml
-oc get projects -w | grep -E "redhat-ods|rhods"
-oc create -f configs/rhoai-operator-dsc.yaml
-oc get DSCInitialization,FeatureTracker -n redhat-ods-operator
-## OpenShift Pipelines
-# Install operator with defaults using UI
-## Red Hat OpenShift Dev Spaces
-# Install operator, create CheCluster with defaults using UI
-oc patch checluster devspaces -n openshift-operators --type='merge' -p='{"spec": {"devEnvironments": {"secondsOfInactivityBeforeIdling": -1,"secondsOfRunBeforeIdling": -1}}}'
+source tools/format.sh
+step=$1
+if [[ -z "$step" || "$step" == "1" ]]; then 
+  __ "ROSA AI Starter configuration tool" 1
+  __ "Set up ROSA cluster using demo.redhat.com - ROSA Workshop" 2
+  ___ "Wait until the environment has been provisioned."
 
-# Create namespace for each team, setup groups and roles
-# - Create Data Science Project
-# - Provision S3 Storage (endpoint requires protocol, valid cert via public url)
-# ?? Github authentication
-# ?? Map each namespace to a worker node
-./rosa-create-users-groups.sh
-# Application Routes
-# Create Workbench
+  __ "Step 1 - Connect to Bastion" 3
+  __ "Collecting initial provisioning data for automation:" 4
+  _? "What is the bastion ssh host for your demo environment" BASTION $BASTION
+  __ "Setup bastion connection and continue there" 4
+  __ "Provide bastion ssh password to copy keys when prompted" 5
+  ssh-copy-id -o StrictHostKeyChecking=accept-new rosa@$BASTION
+  __ "Connect to bastion via ssh using -A flag" 4
+  # Checkout dependencies
+  ssh -A rosa@$BASTION 'GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=accept-new" git clone ssh://git@gitlab.consulting.redhat.com:2222/ai-odyssey-2025/assist4real/demo-project.git ai-starter'
+  # rsync -va ./ rosa@$BASTION:ai-starter/
+  ssh -A rosa@$BASTION "cd ai-starter && pwd && ./setup.sh 2"
+  exit 0
+fi
+clusterInfo=$(rosa list clusters -o json)
+if [[ -n "$step" && "$step" == "2" ]]; then 
+  __ "Step 2 - Setup ROSA" 3
+  API_URL=$(echo "$clusterInfo" | jq -r '.[].api.url')
+  __ "Create ROSA admin account - should exist" 4
+  cmd "rosa create admin --cluster rosa-$GUID"
+  __ "Collecting additional provisioning data for automation:" 4
+  _? "What is the rosa admin password" API_PWD
+  _? "What is the rosa api url" API_URL "" $API_URL
+  cmd oc login -u cluster-admin -p "$API_PWD" "$API_URL"
+  __ "Setup scratch folder for artifacts" 4
+  cmd "mkdir -p scratch/"
+  step=3
+fi
+if [[ -n "$step" && "$step" == "3" ]]; then 
+  __ "Step 3 - Configure ROSA machine pool" 3
+  _? "What is the instance type to use" instanceType g5.4xlarge
+  _? "What is the number of minimum replicas" minReplicas 2
+  _? "What is the number of maximum replicas" maxReplicas 10
+  __ "Add $instanceType machine pool with $minReplicas <= n <= $maxReplicas nodes" 4
+  cmd "rosa create machinepool -c rosa-$GUID --name=ai-worker --min-replicas=$minReplicas --max-replicas=$maxReplicas --instance-type=$instanceType --enable-autoscaling --labels nodes=ai"
+  step=4
+fi
+if [[ -n "$step" && "$step" == "4" ]]; then 
+  # todo: while recommended version, upgrade
+  __ "Step 4 - Upgrade ROSA" 3
+  currentRosaVersion=$(echo "$clusterInfo" | jq -r '.[].openshift_version')
+  nextRosaVersion=$(rosa list upgrades -c rosa-$GUID -o json | jq -r '.[0]')
+  __ "Current     ROSA version: $currentRosaVersion" 5
+  __ "Recommended ROSA version: $nextRosaVersion" 5
+  _? "What version of ROSA" rosaVersion $nextRosaVersion
+  cmd rosa upgrade cluster -c rosa-$GUID --control-plane --schedule-date $(date -d "+5 minutes 30 seconds" +"%Y-%m-%d") --schedule-time $(date -d "+6 minutes" +"%H:%M") -m auto -y --version $rosaVersion 
+  __ "Wait for upgrade to finish" 4
+  oo 1 "echo \$(( 1 - \$(rosa list upgrades -c rosa-$GUID | grep recommended | grep '$rosaVersion' | wc -l) ))"
+  step=5
+fi
+if [[ -n "$step" && "$step" == "5" ]]; then 
+  __ "Step 5 - Finish Openshift Setup" 3
+  __ "Wait for machinepool to be ready" 4
+  export query='.[] | select(.id=="ai-worker") .status.current_replicas'
+  oo $(rosa list machinepools -c rosa-$GUID -o json | jq '.[] | select(.id=="ai-worker") .autoscaling.min_replica') "rosa list machinepools -c rosa-$GUID -o json | jq '$query'"
+  unset query
+  __ "Switch to AI machine pool" 4
+  cmd "rosa update machinepool -c rosa-$GUID --replicas 0 workers"
+  __ "Verify machine pools" 4
+  cmd "rosa list machinepools -c rosa-$GUID"
+  step=6
+fi
+if [[ -n "$step" && "$step" == "6" ]]; then 
+  __ "Set up OpenShift AI" 2
+  __ "Step 6 - Install Operators" 3
+  __ "Web Terminal Operator" 4
+  cmd oc apply -f configs/web-terminal-subscription.yaml
+  __ "OpenShift Service Mesh" 4
+  cmd oc create ns istio-system
+  cmd oc create -f configs/servicemesh-subscription.yaml
+  __ "OpenShift Serverless" 4
+  cmd oc create -f configs/serverless-operator.yaml
+  __ "Authorino" 4
+  cmd oc create -f configs/authorino-subscription.yaml
+  __ "Verify dependencies" 4
+  cmd oc get subscriptions -A
+  __ "OpenShift AI >2.11 via OLM on ROSA" 4
+  cmd oc create -f configs/rhoai-operator-ns.yaml
+  cmd oc create -f configs/rhoai-operator-group.yaml
+  cmd oc create -f configs/rhoai-operator-subscription.yaml
+  __ "Verify dependencies are installed" 5
+  oo 3 'oc get projects | grep -E "redhat-ods|rhods" | wc -l'
+  cmd oc create -f configs/rhoai-operator-dsc.yaml
+  __ "Verify dependencies are installed" 5
+  oo 9 "oc get DSCInitialization,FeatureTracker -n redhat-ods-operator | egrep -i 'DSCInitialization|FeatureTracker' | grep -iv Progressing | wc -l"
+  cmd oc get DSCInitialization,FeatureTracker -n redhat-ods-operator
+  __ "OpenShift Pipelines" 4
+  cmd "oc apply -f configs/pipelines-subscription.yaml"
+  oo 1 "oc get ClusterServiceVersion -l operators.coreos.com/openshift-pipelines-operator-rh.openshift-operators -n openshift-operators 2>/dev/null | grep Succeeded | wc -l"
+  __ "Red Hat OpenShift Dev Spaces" 4
+  cmd "oc apply -f configs/dev-spaces-subscription.yaml"
+  oo 1 "oc get ClusterServiceVersion -l operators.coreos.com/devworkspace-operator.openshift-operators -n openshift-operators 2>/dev/null | grep Succeeded | wc -l"
+  __ "Wait for devspaces-operator-service to be present" 5
+  oo 1 "oc get service devspaces-operator-service -n openshift-operators --no-headers=true 2>/dev/null | wc -l"
+  __ "Wait for devspaces-operator service to be ready" 5
+  cmd "oc wait pod -l app=devspaces-operator -n openshift-operators --for=condition=Ready"
+  __ "Create CheCluster" 4
+  cmd "oc apply -f configs/dev-spaces-instance.yaml"
+  oo 1 "oc get CheCluster devspaces -n openshift-devspaces -o name --no-headers=true | wc -l"
+  __ "Patch CheCluster to never idle" 4
+  patch='{"spec": {"devEnvironments": {"secondsOfInactivityBeforeIdling": -1,"secondsOfRunBeforeIdling": -1}}}'
+  cmd "oc patch checluster devspaces -n openshift-devspaces --type='merge' -p='$patch'"
+  step=7
+fi
+# Have a default storage class
+if [[ -n "$step" && "$step" == "7" ]]; then 
+  __ "Set up Teams" 2
+  __ "Step 7 - Create namespace for each team, setup groups and roles" 3
+  __ "Provision S3 Storage (endpoint requires protocol, valid cert via public url)" 4
+  __ "Create groups for each team with 10 users" 5
+  __ "Create Data Science Project" 6
+  __ "Application Routes" 6
+  __ "Create Workbench" 6
+  cmd ./rosa-create-users-groups.sh 3
+fi
+exit 0;
 # Available images: oc get imagestream -n redhat-ods-applications
